2edbf3188f42133ac4ee6e236bf7a18a0d2b58ab
Victor Fernández <victorfernandezrd@gmail.com>
2025-06-11 11:25:43 -0300

    Se cambia el cálculo dv del CDC

--- src/Utils/GenerarCDC.cs
+++ src/Utils/GenerarCDC.cs
@@ -5,10 +5,10 @@ public class GenerarCDC
     public static string GenerarCodigoCDC(string iTiDE, string dRucEm, string dDVEmi, string dEst, string dPunExp, string dNumDoc, string dTipCont, string dFeEmiDE, string iTipEmi, string dCodSeg)
     {
         string cdcBase = $"{iTiDE.Trim()}{dRucEm.Trim()}{dDVEmi.Trim()}{dEst.Trim()}{dPunExp.Trim()}{dNumDoc.Trim()}{dTipCont.Trim()}{dFeEmiDE.Trim()}{iTipEmi.Trim()}{dCodSeg.Trim()}";
-        Console.WriteLine($"CDC base generado: {cdcBase} " );
+        Console.WriteLine($"CDC base generado: {cdcBase} ");
         int dv = CalcularDV(cdcBase);
-    /*    Console.WriteLine($"Dígito verificador: {dv}");*/
-        Console.WriteLine($"CDC + dígito verificador: {cdcBase},{dv}"); 
+        /*    Console.WriteLine($"Dígito verificador: {dv}");*/
+        Console.WriteLine($"CDC + dígito verificador: {cdcBase},{dv}");
         return cdcBase + dv;
     }
 
@@ -32,6 +32,7 @@ public class GenerarCDC
         return 11 - resto;
     }
     */
+    /*
 
     private static int CalcularDV(string cdcBase)
     {
@@ -39,17 +40,47 @@ public class GenerarCDC
         int[] pesos = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
         int suma = 0;
         int longitud = cdcBase.Length;
-        
-        for (int i = 0; i < longitud; i++) 
+
+        for (int i = 0; i < longitud; i++)
         {
             int digito = int.Parse(cdcBase[longitud - 1 - i].ToString());
             int peso = pesos[i % 10];
             suma += digito * peso;
         }
-        
+
         int resto = suma % 11;
         if (resto == 0) return 0;
         if (resto == 1) return 1;
         return 11 - resto;
+    } */
+    
+    private static int CalcularDV(string cdcBase)
+    {
+        string numeroConvertido = "";
+        foreach (char c in cdcBase.ToUpper())
+        {
+            if (char.IsDigit(c))
+            {
+                numeroConvertido += c;
+            }
+            else
+            {
+                numeroConvertido += ((int)c).ToString();
+            }
+        }
+
+        int total = 0;
+        int k = 2;
+        for (int i = numeroConvertido.Length - 1; i >= 0; i--)
+        {
+            if (k > 11) k = 2;
+            int digito = int.Parse(numeroConvertido[i].ToString());
+            total += digito * k;
+            k++;
+        }
+
+        int resto = total % 11;
+        int dv = (resto > 1) ? (11 - resto) : 0;
+        return dv;
     }
 }
